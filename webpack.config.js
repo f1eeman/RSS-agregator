e6df83f259aa553e8d4b43e347327b98239085b8
f1eeman <mr.f1eeman@gmail.com>
2020-09-06 02:11:12 +0600

    попытка настроить webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import './src/style.css';
+import './src/index';
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.js$/, use: 'babel-loader' },
+    ],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
