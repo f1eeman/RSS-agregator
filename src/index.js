b3ad4ffbff8629b7f8a2c8f4fe992ce56aef05fa
f1eeman <mr.f1eeman@gmail.com>
2020-09-07 02:06:22 +0600

    Пробный коммит

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import 'bootstrap/js/dist/alert';
+
+const bodyEl = document.body;
+const container = document.createElement('div');
+container.classList.add('container');
+bodyEl.append(container);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import './src/style.css';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// import path from 'path';
+// import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 module.exports = {
   entry: './src/index.js',
